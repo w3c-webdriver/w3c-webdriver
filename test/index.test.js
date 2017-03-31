ff2cd97fafa9b21afa87bbdbc80852ca21106fb9
Igor Mucsicska <mucsi_96@yahoo.com>
2017-03-31 13:54:54 +0200

    Chai added

--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,29 @@
+const chai = require('chai');
+chai.should();
 const webDriver = require('../src');
 
 let session;
 const baseUrl = 'http://localhost:9515';
 
 async function test() {
-  const session = await webDriver(baseUrl, {
-    browserName: 'Chrome'
-  });
-  await session.go('http://localhost:8087');
-  const title = await session.getTitle();
-  console.log(title);
-  await (await session.findElement('#a')).sendKeys('15');
-  await (await session.findElement('#b')).sendKeys('6');
-  await (await session.findElement('#add')).click();
-  const text = await (await session.findElement('#result')).getText();
-  console.log(text);
-  await session.deleteSession();
+  let session;
+  try {
+    session = await webDriver(baseUrl, {
+      browserName: 'Chrome'
+    });
+    await session.go('http://localhost:8087');
+    const title = await session.getTitle();
+    console.log(title);
+    await (await session.findElement('#a')).sendKeys('15');
+    await (await session.findElement('#b')).sendKeys('6');
+    await (await session.findElement('#add')).click();
+    const text = await (await session.findElement('#result')).getText();
+    text.should.equal('21');
+  } catch (err) {
+    throw err;
+  } finally {
+    await session.deleteSession();
+  }
 }
 
 
