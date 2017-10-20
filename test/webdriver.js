6a7192dc8b77a7e54c573658fca273d75d9c942a
Igor Muchychka <mucsi_96@yahoo.com>
2017-10-20 20:53:29 +0200

    Improve start/stop of webdriver

--- test/webdriver.js
+++ test/webdriver.js
@@ -5,13 +5,23 @@ const { path: geckodriverPath } = require('geckodriver');
 const { path: phantomjsPath } = require('phantomjs-prebuilt');
 const { path: iedriverPath } = require('iedriver');
 const logger = require('./logger');
+const waitForPort = require('wait-for-port');
+
+const waitForBusyPort = port => new Promise((resolve, reject) => {
+  waitForPort('127.0.0.1', port, err => (err ? reject(err) : resolve()));
+});
+
+const waitForFreePort = port => new Promise((resolve, reject) => {
+  waitForPort('127.0.0.1', port, { reverse: true }, err => (err ? reject(err) : resolve()));
+});
 
 let instance;
+let usedPort;
 
 const browser = process.env.BROWSER;
 assert(browser, 'BROWSER environment variable is not set');
 
-function start(port) {
+async function start(port) {
   const childArgs = {
     chrome: [`--port=${port}`],
     'chrome-headless': [`--port=${port}`],
@@ -33,44 +43,24 @@ function start(port) {
     phantomjs: 'GhostDriver',
     'internet-explorer': 'InternetExplorerDriver'
   }[browser];
-  const ready = {
-    chrome: 'on port',
-    'chrome-headless': 'on port',
-    firefox: 'Listening on',
-    phantomjs: 'running on port',
-    'internet-explorer': 'Only local connections are allowed'
-  }[browser];
 
-  logger.info(`[webdriver:start] path: ${path}, childArgs: ${childArgs}`);
-  return new Promise((resolve, reject) => {
-    let started = false;
-    const chunks = [];
-    instance = execFile(path, childArgs);
-    const onClose = () => {
-      logger.info(`[webdriver:start] ${name} terminated`);
-      if (!started) reject();
-    };
-    const onOut = (chunk) => {
-      logger.info(`[webdriver] ${chunk}`);
-      chunks.push(chunk);
-      if (!started && chunks.join('').includes(ready)) {
-        logger.info(`[webdriver:start] ${name} started on port ${port}`);
-        /* eslint-disable no-use-before-define */
-        done();
-      }
-    };
-    const done = () => {
-      started = true;
-      resolve();
-    };
-    instance.stdout.on('data', onOut);
-    instance.stderr.on('data', onOut);
-    instance.on('close', onClose);
-  });
+  logger.info(`[webdriver:start] Starting ${name} ${path} ${childArgs.join(' ')}`);
+  instance = execFile(path, childArgs);
+  const onClose = () => logger.info(`[webdriver:start] ${name} terminated`);
+  const onOut = chunk => logger.info(`[webdriver] ${chunk}`);
+  instance.stdout.on('data', onOut);
+  instance.stderr.on('data', onOut);
+  instance.on('close', onClose);
+  await waitForBusyPort(port);
+  usedPort = port;
+  logger.info(`[webdriver:start] ${name} started on port ${port}`);
 }
 
-function stop() {
-  if (instance) instance.kill();
+async function stop() {
+  if (usedPort) {
+    instance.kill();
+    await waitForFreePort(usedPort);
+  }
 }
 
 module.exports = {
