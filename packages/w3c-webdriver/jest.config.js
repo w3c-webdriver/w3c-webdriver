b940da471cb26bac009dceb8918e96dfe1294d80
Igor Mucsicska <mucsi_96@yahoo.com>
2019-05-05 22:35:57 +0200

    Simplify CI tets execution

--- packages/w3c-webdriver/jest.config.js
+++ packages/w3c-webdriver/jest.config.js
@@ -2,12 +2,8 @@ module.exports = {
   globalSetup: './test-env/globalSetup.ts',
   globalTeardown: './test-env/globalTeardown.ts',
   setupFilesAfterEnv: ['./test-env/setup.ts'],
-  reporters: ['default', process.env.CI && 'jest-junit'].filter(Boolean),
   collectCoverageFrom: [
     "<rootDir>/src/**/*.{js,ts}",
     "!<rootDir>/src/**/*.test.{js,ts}"
-  ],
-  coverageReporters: ['html', process.env.CI && 'cobertura'].filter(Boolean),
-  ...(process.env.CI && { collectCoverage: true }),
-  ...(process.env.BROWSER === 'safari' && { maxConcurrency: 1 })
+  ]
 };
