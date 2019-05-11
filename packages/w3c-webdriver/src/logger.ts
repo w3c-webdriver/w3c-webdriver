d0c46d3694ba5aa6078d7890ed7435c2e548c38a
Igor Mucsicska <mucsi_96@yahoo.com>
2019-05-11 23:31:19 +0200

    Move Chrome installation to pipelines

--- packages/w3c-webdriver/src/logger.ts
+++ packages/w3c-webdriver/src/logger.ts
@@ -1,10 +1,4 @@
 import debug from 'debug';
-import { format } from 'util';
 
 // tslint:disable-next-line:export-name
 export const log = debug('w3c-webdriver');
-
-// tslint:disable-next-line: no-any
-log.log = (formatt: any, ...param: any[]) => {
-	return process.stdout.write(`${format(formatt, ...param)}\n`);
-}
